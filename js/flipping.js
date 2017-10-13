ab7f1497a50b02840bf86973bee7f0ce8c8fd0c4
Max <max@develexe.com>
2017-10-13 17:27:27 +0300

    arrows css updated

--- js/flipping.js
+++ js/flipping.js
@@ -1,120 +1,122 @@
-window.onload = function () {
+(function () {
 
-    var a = document.getElementById('left');
-    a.onclick = function () {
-        backward();
-        return true;
-    }
-
-    var a = document.getElementById('right');
-    a.onclick = function () {
-        forward();
-        return true;
-    }
+    window.onload = function () {
 
 
-    var page = 0;
+        var a = document.getElementById('left');
+        a.onclick = function () {
+            backward();
+            return true;
+        }
 
-    var imgfr;
-    var imgbk;
-
-    var images = [];
-
-    images[0] = ["images/1.jpg", "images/4.jpg", "images/7.jpg", "images/10.jpg", "images/13.jpg"];
-    images[1] = ["images/2.jpg", "images/5.jpg", "images/8.jpg", "images/11.jpg", "images/14.jpg"];
-    images[2] = ["images/3.jpg", "images/6.jpg", "images/9.jpg", "images/12.jpg", "images/15.jpg"];
-
-
-    var flipping_cards = document.getElementById('flipping_cards');
-    var cards = flipping_cards.getElementsByClassName('card');
-
-    // prepare cards
-    for (var i = 0; i < cards.length; i++) {
-
-        var fr = cards[i].getElementsByClassName('front')[0].style;
-        var bk = cards[i].getElementsByClassName('back')[0].style;
-
-        bk.display = 'block';
-        fr.backgroundImage = 'url(' + images[i][0] + ')';
-        bk.transform = 'rotateY(180deg)';
-
-    }
+        var a = document.getElementById('right');
+        a.onclick = function () {
+            forward();
+            return true;
+        }
 
 
-    // next
-    function forward() {
+        var page = 0;
 
+        var imgfr;
+        var imgbk;
+
+        var images = [];
+
+        images[0] = ["images/1.jpg", "images/4.jpg", "images/7.jpg", "images/10.jpg", "images/13.jpg"];
+        images[1] = ["images/2.jpg", "images/5.jpg", "images/8.jpg", "images/11.jpg", "images/14.jpg"];
+        images[2] = ["images/3.jpg", "images/6.jpg", "images/9.jpg", "images/12.jpg", "images/15.jpg"];
+
+        var flipping_cards = document.getElementById('flipping_cards');
+        var cards = flipping_cards.getElementsByClassName('card');
+
+        // prepare cards
         for (var i = 0; i < cards.length; i++) {
 
             var fr = cards[i].getElementsByClassName('front')[0].style;
             var bk = cards[i].getElementsByClassName('back')[0].style;
 
-            // if last
-            if (page >= images[i].length - 1) {
-                page = images[i].length - 1;
-                return;
-            }
-
-            if (page % 2) {
-                imgfr = page + 1;
-                imgbk = page;
-            }
-            else {
-                imgfr = page;
-                imgbk = page + 1;
-            }
-
-
-            fr.backgroundImage = 'url(' + images[i][imgfr] + ')';
-            bk.backgroundImage = 'url(' + images[i][imgbk] + ')';
-            fr.transform = 'rotateY(' + (-180 * (page + 1)) + 'deg)';
-            bk.transform = 'rotateY(' + (-180 * page) + 'deg)';
+            bk.display = 'block';
+            fr.backgroundImage = 'url(' + images[i][0] + ')';
+            bk.transform = 'rotateY(180deg)';
 
         }
 
-        page = page + 1;
-    }
 
-    // prev
-    function backward() {
+        // next
+        function forward() {
 
-        for (var i = 0; i < cards.length; i++) {
+            for (var i = 0; i < cards.length; i++) {
 
-            var fr = cards[i].getElementsByClassName('front')[0].style;
-            var bk = cards[i].getElementsByClassName('back')[0].style;
+                var fr = cards[i].getElementsByClassName('front')[0].style;
+                var bk = cards[i].getElementsByClassName('back')[0].style;
 
-            // if first
-            if (page <= 0) {
-                page = 0;
-                return;
+                // if last
+                if (page >= images[i].length - 1) {
+                    page = images[i].length - 1;
+                    return;
+                }
+
+                if (page % 2) {
+                    imgfr = page + 1;
+                    imgbk = page;
+                }
+                else {
+                    imgfr = page;
+                    imgbk = page + 1;
+                }
+
+                fr.backgroundImage = 'url(' + images[i][imgfr] + ')';
+                bk.backgroundImage = 'url(' + images[i][imgbk] + ')';
+                fr.transform = 'rotateY(' + (-180 * (page + 1)) + 'deg)';
+                bk.transform = 'rotateY(' + (-180 * page) + 'deg)';
+
             }
 
-            if (page % 2) {
-                imgfr = page - 1;
-                imgbk = page;
-            }
-            else {
-                imgfr = page;
-                imgbk = page - 1;
-            }
-
-
-            fr.backgroundImage = 'url(' + images[i][imgfr] + ')';
-            bk.backgroundImage = 'url(' + images[i][imgbk] + ')';
-            fr.transform = 'rotateY(' + (-180 * (page - 1)) + 'deg)';
-            if (page > 1) {
-                bk.transform = 'rotateY(' + (-180 * (page - 2)) + 'deg)';
-            }
-            else {
-                bk.transform = 'rotateY(' + 180 + 'deg)';
-            }
-
+            page = page + 1;
         }
-        page = page - 1;
+
+        // prev
+        function backward() {
+
+            for (var i = 0; i < cards.length; i++) {
+
+                var fr = cards[i].getElementsByClassName('front')[0].style;
+                var bk = cards[i].getElementsByClassName('back')[0].style;
+
+                // if first
+                if (page <= 0) {
+                    page = 0;
+                    return;
+                }
+
+                if (page % 2) {
+                    imgfr = page - 1;
+                    imgbk = page;
+                }
+                else {
+                    imgfr = page;
+                    imgbk = page - 1;
+                }
+
+                fr.backgroundImage = 'url(' + images[i][imgfr] + ')';
+                bk.backgroundImage = 'url(' + images[i][imgbk] + ')';
+                fr.transform = 'rotateY(' + (-180 * (page - 1)) + 'deg)';
+                if (page > 1) {
+                    bk.transform = 'rotateY(' + (-180 * (page - 2)) + 'deg)';
+                }
+                else {
+                    bk.transform = 'rotateY(' + 180 + 'deg)';
+                }
+
+            }
+            page = page - 1;
+        }
+
     }
 
-}
 
+})(this);
 
 
 
